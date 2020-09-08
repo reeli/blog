262460a2e0ba99835c3bbfd07faa27fc21556a0b
reeli <reeli.null@gmail.com>
2020-09-08 14:30:06 +0800

    add ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: "橘子的前端杂记",
   description: "个人博客。记录实际开发中遇到的问题及解决方案，日常的思考和总结、面试知识点、读书笔记等。",
-  ga: "UA-177434976-1",
   markdown: {
     lineNumbers: true
   },
@@ -38,6 +37,12 @@ module.exports = {
     }
   },
   plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        "ga": "UA-177434976-1"
+      }
+    ],
     [
       "@vuepress/pwa",
       {
